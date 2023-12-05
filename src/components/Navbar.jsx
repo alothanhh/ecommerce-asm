fc4617b75a3a7ff3a8beadd45951cddd8b9654fb
Huy Dang <huy.dang110902@gmail.com>
2023-12-05 22:29:38 +0700

    Rebase 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,52 +1,116 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Navbar = () => {
-    const state = useSelector(state => state.handleCart)
-    return (
-        <nav className="navbar navbar-expand-lg navbar-white bg-white py-3 sticky-top">
-            <div className="container">
-                <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">
-                    <img src="./assets/icon.jpg" alt="Sourique corner" style={{ width: '200px', height: 'auto' }} />
+  const navigate = useNavigate();
+  const state = useSelector((state) => state.handleCart);
+  const [currentUser, setCurrentUser] = useState(() => {
+    const user = localStorage.getItem("userProfile");
+    if (user) {
+      return true;
+    }
+    return false;
+  });
+
+  const HandleLogout = async () => {
+    setCurrentUser(false);
+    localStorage.clear();
+    try {
+      const response = await axios("localhost:8000/api/v1/users/logout/");
+      if (response.data.message === "Good Bye") {
+        navigate("/");
+      }
+    } catch (error) {}
+  };
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-white bg-white py-3 sticky-top">
+      <div className="container">
+        <NavLink className="navbar-brand fw-bold fs-4 px-2" to="/">
+          <img
+            src="../assets/icon.png"
+            alt="Sourique corner"
+            style={{ width: "200px", height: "auto" }}
+          />
+        </NavLink>
+        <button
+          className="navbar-toggler mx-2"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav m-auto my-2 text-center">
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/">
+                Trang chủ{" "}
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/product">
+                Sản phẩm
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/about">
+                Về chúng tôi
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/contact">
+                Liên hệ
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/checkerror">
+                Kiểm lỗi
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/chatbox">
+                Tư vấn KH
+              </NavLink>
+            </li>
+          </ul>
+          <div className="buttons text-center">
+            {!currentUser && (
+              <>
+                <NavLink to="/login" className="btn btn-outline-dark m-2">
+                  <i className="fa fa-sign-in-alt mr-1"></i> Đăng nhập
                 </NavLink>
-                <button className="navbar-toggler mx-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+                <NavLink to="/register" className="btn btn-outline-dark m-2">
+                  <i className="fa fa-user-plus mr-1"></i> Đăng ký
+                </NavLink>
+              </>
+            )}
+            {currentUser && (
+              <>
+                <button
+                  onClick={HandleLogout}
+                  className="btn btn-outline-dark m-2"
+                >
+                  <i className="fa fa-sign-out-alt mr-1"></i> Đăng xuất
                 </button>
+                <NavLink to="/cart" className="btn btn-outline-dark m-2">
+                  <i className="fa fa-cart-shopping mr-1"></i> Giỏ hàng (
+                  {state.length}){" "}
+                </NavLink>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav m-auto my-2 text-center">
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/">Trang chủ </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/product">Sản phẩm</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/about">Về chúng tôi</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/contact">Liên hệ</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/checkerror">Kiểm lỗi</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link" to="/chatbox">Tư vấn KH</NavLink>
-                        </li>
-                    </ul>
-                    <div className="buttons text-center">
-                        <NavLink to="/login" className="btn btn-outline-dark m-2"><i className="fa fa-sign-in-alt mr-1"></i> Đăng nhập</NavLink>
-                        <NavLink to="/register" className="btn btn-outline-dark m-2"><i className="fa fa-user-plus mr-1"></i> Đăng ký</NavLink>
-                        <NavLink to="/cart" className="btn btn-outline-dark m-2"><i className="fa fa-cart-shopping mr-1"></i> Giỏ hàng ({state.length}) </NavLink>
-                    </div>
-                </div>
-
-
-            </div>
-        </nav>
-    )
-}
-
-export default Navbar
+export default Navbar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,22 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import { Footer, Navbar } from "../components";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+  const [notify, setNotify] = useState(false);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/api/v1/users/login/",
+        form
+      );
+      if (response.data.message === "Wrong Authetication") {
+        setNotify(true);
+      } else {
+        setNotify(false);
+        localStorage.setItem("userProfile", JSON.stringify(response.data));
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleForm = (event) => {
+    const { name, value } = event.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
   return (
     <>
       <Navbar />
       <div className="container my-3 py-3">
         <h1 className="text-center">Đăng nhập</h1>
         <hr />
-        <div class="row my-4 h-100">
+        {notify && (
+          <div className="col-md-4 col-lg-4 col-sm-8 mx-auto text-center">
+            <p>Sai email hoặc password, Xin hãy nhập lại!</p>
+          </div>
+        )}
+        <div class="row my-4 h-100 ">
           <div className="col-md-4 col-lg-4 col-sm-8 mx-auto">
-            <form>
+            <form onSubmit={handleLogin}>
               <div class="my-3">
                 <label for="display-4">Địa chỉ Email</label>
                 <input
                   type="email"
+                  name="email"
+                  value={form.email}
                   class="form-control"
+                  onChange={handleForm}
                   id="floatingInput"
                   placeholder="name@gmail.com"
                 />
@@ -25,16 +68,27 @@ const Login = () => {
                 <label for="floatingPassword display-4">Mật khẩu</label>
                 <input
                   type="password"
+                  name="password"
+                  value={form.password}
                   class="form-control"
+                  onChange={handleForm}
                   id="floatingPassword"
                   placeholder="Password"
                 />
               </div>
               <div className="my-3">
-                <p>Bạn chưa có tài khoản? <Link to="/register" className="text-decoration-underline text-info">Đăng ký ở đây</Link> </p>
+                <p>
+                  Bạn chưa có tài khoản?{" "}
+                  <Link
+                    to="/register"
+                    className="text-decoration-underline text-info"
+                  >
+                    Đăng ký ở đây
+                  </Link>{" "}
+                </p>
               </div>
               <div className="text-center">
-                <button class="my-2 mx-auto btn btn-dark" type="submit" disabled>
+                <button class="my-2 mx-auto btn btn-dark" type="submit">
                   Đăng nhập
                 </button>
               </div>
